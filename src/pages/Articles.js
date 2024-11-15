d5cd8894658188c4222d32f11a67062464ce2081
HetarthJain <hetarth.jain@gmail.com>
2024-11-15 12:43:16 +0530

    fix

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -7,7 +7,7 @@ function Articles({ articles }) {
                 <div className="article-comp">
                     <h1>My Articles</h1>
                     <div className="article">
-                    <Link to={`/article/${article.id}`} className="article-title">
+                    <Link to="https://theprint.in/campus-voice/india-lacks-an-intellectual-economic-hindu-right-wing-bjp-does-not-qualify/1174580/" className="article-title">
                             <p>{article.title}</p>
                         </Link>
                         <span className="article-date">{article.date}</span>
